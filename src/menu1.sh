b5e86a2b21b3e7b09155d61f6fe374ad11dd59c1
jingse <1559572379@qq.com>
2015-11-03 19:05:24 +0800

    Create menu1.sh

---
+++ src/menu1.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+bash ./locate_cursor.sh 6 20
+bash ./color_output.sh "+++++++++++++++++++++++++++++++++" sblue
+bash ./locate_cursor.sh 7 20
+bash ./color_output.sh "+           开心扫雷            +" sblue
+bash ./locate_cursor.sh 8 20
+bash ./color_output.sh "+++++++++++++++++++++++++++++++++               *   *              *   *" sblue
+bash ./locate_cursor.sh 9 20
+bash ./color_output.sh "+                               +              *       *        *       *" sblue
+bash ./locate_cursor.sh 10 20 
+bash ./color_output.sh "+           1->简单             +                *   *   *    *   *   *" sblue
+bash ./locate_cursor.sh 11 20
+bash ./color_output.sh "+                               +                         *  *" sblue
+bash ./locate_cursor.sh 12 20 
+bash ./color_output.sh "+           2->中等             +                         *  *" sblue
+bash ./locate_cursor.sh 13 20
+bash ./color_output.sh "+                               +                        *    *" sblue
+bash ./locate_cursor.sh 14 20 
+bash ./color_output.sh "+           3->困难             +                       *      *" sblue
+bash ./locate_cursor.sh 15 20
+bash ./color_output.sh "+                               +                      *        *       *" sblue
+bash ./locate_cursor.sh 16 20 
+bash ./color_output.sh "+           4->退出             +                    *            *      *" sblue
+bash ./locate_cursor.sh 17 20
+bash ./color_output.sh "+                               +                  *                *  *" sblue
+bash ./locate_cursor.sh 18 20 
+bash ./color_output.sh "+++++++++++++++++++++++++++++++++                *" sblue
+bash ./locate_cursor.sh 19 20 
+bash ./color_output.sh "                 *    *                        *" sblue
+bash ./locate_cursor.sh 20 20 
+bash ./color_output.sh "               *         *                   *" sblue
+bash ./locate_cursor.sh 21 20 
+bash ./color_output.sh "                  *   *     *              *" sblue
+bash ./locate_cursor.sh 22 20 
+bash ./color_output.sh "                              *          *" sblue
+bash ./locate_cursor.sh 23 20 
+bash ./color_output.sh "                                 *     *" sblue
+bash ./locate_cursor.sh 24 20 
+bash ./color_output.sh "                                   * *" sblue
+bash ./locate_cursor.sh 25 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 26 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 27 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 28 20  
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 29 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 30 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 31 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 32 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 33 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 34 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 35 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 36 20 
+bash ./color_output.sh "                                    *" sblue
+bash ./locate_cursor.sh 55 0
+
